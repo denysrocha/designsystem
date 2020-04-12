46feaba19f444a96066eab53e093280d3b810db0
Denys Rocha <getraknexcorp@MacBook-Pro-de-Denys.local>
2020-04-12 11:23:38 -0300

    Aplicação rodando com os átomos

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <button-primary />
+  </div>
+</template>
+
+<script>
+import ButtonPrimary from './atomos/botao/ButtonPrimary.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ButtonPrimary
+  }
+}
+</script>
+
---
+++ src/atomos/botao/ButtonPrimary.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button-primary" v-on:click="onClick">Button Primary {{counter}}</button>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    onClick() {
+      this.counter += 1;
+    }
+  }
+};
+</script>
+<style>
+.button-primary {
+  color: #FFFFFF;
+  background: #2980b9;
+  padding: 8px 12px;
+  border-radius: 4px;
+  border: none;
+  font-weight: 500;
+}
+.button-primary:hover {
+  background: #3498db;
+}
+</style>
+
+
