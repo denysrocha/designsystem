74f1d8291d63a9390bb2962fddafbc53b71a2551
Denys Rocha <getraknexcorp@MacBook-Pro-de-Denys.local>
2020-04-27 16:06:42 -0300

    lint build docs

--- build-docs.js
+++ build-docs.js
@@ -4,28 +4,28 @@ const fs = require('fs');
 
 // options is optional
 glob(`${testFolder}/**/*.vue`, function (er, files) {
-    // files is an array of filenames.
-    // If the `nonull` option is set, and nothing
-    // was found, then files is ["**/*.js"]
-    // er is an error object or null.
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
 
-    files.map(file => {
-        const splitedPath = file.split('/')
-        const component = splitedPath.pop()
-        const componentPath = splitedPath.join('/').replace('./src/','')
-        const samplePath = `./exemplos/${componentPath}/${component.replace('.vue','')}`
+  files.map(file => {
+      const splitedPath = file.split('/')
+      const component = splitedPath.pop()
+      const componentPath = splitedPath.join('/').replace('./src/','')
+      const samplePath = `./exemplos/${componentPath}/${component.replace('.vue','')}`
 
-        console.info(`### Creating sample for ${component} ###`)
-        fs.rmdir(samplePath, { recursive: true }, (err) => {
-            fs.mkdir(samplePath, { recursive: true }, (err) => {
+      console.info(`### Creating sample for ${component} ###`)
+      fs.rmdir(samplePath, { recursive: true }, (err) => {
+          fs.mkdir(samplePath, { recursive: true }, (err) => {
 
-                fs.copyFile(file, `${samplePath}/${component}`, (err) => {
-                    if (err) throw err;
-                    console.log(`${file} copied!`);
-                  });
+            fs.copyFile(file, `${samplePath}/${component}`, (err) => {
+                if (err) throw err;
+                console.log(`${file} copied!`);
+              });
 
-                const data = {
-                    'Wrapper.vue' : `<template>
+            const data = {
+                  'Wrapper.vue' : `<template>
     <div class="wrapper">
         <generic-component />
     </div>
@@ -51,7 +51,7 @@ export default {
 }
 </style>
                 
-                `,
+            `,
                     'readme.md': 'This folders were created automatically',
                     'index.html': '<div id="app"></div>',
                     'package.json': `{
@@ -75,7 +75,7 @@ export default {
   "keywords": [],
   "description": "Item do Design System"
 }
-                 `,
+            `,
                     'index.js': `import Vue from "vue";
 import App from "./Wrapper.vue";
 Vue.config.productionTip = false;
@@ -84,21 +84,20 @@ new Vue({
   template: "<App/>",
   components: { App }
 });`
-                }
+          }
 
-                // creating files
-                for (fileName in data) {
-                    const fileContent = new Uint8Array(Buffer.from(data[fileName]))
-                    const sampleName = `${samplePath}/${fileName}`
+          // creating files
+          for (fileName in data) {
+            const fileContent = new Uint8Array(Buffer.from(data[fileName]))
+            const sampleName = `${samplePath}/${fileName}`
 
-                    fs.writeFile(sampleName, fileContent, (err) => {
-                        if (err) throw err;
-                        console.log(`${sampleName} created!`)
-                    });
-                }
+            fs.writeFile(sampleName, fileContent, (err) => {
+              if (err) throw err;
+              console.log(`${sampleName} created!`)
             });
-        })
-
-        return file
+          }
+      });
     })
+    return file
+  })
 })
